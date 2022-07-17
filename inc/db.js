a1df09b005664442bb94bb2b62dadc237a58ef6f
Matheus Khairallah <61334878+MatheusKhairallah@users.noreply.github.com>
2022-07-17 14:06:57 -0300

    finished project

--- inc/db.js
+++ inc/db.js
@@ -3,8 +3,9 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'matheus',
+    password: 'clear',
     database: 'saboroso',
-    password: 'clear'
+    multipleStatements: true
 });
 
 module.exports = connection;
